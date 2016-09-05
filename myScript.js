98a81d6ee692bdc30877aa254d580ad6d069b223
grayjaim <jaime.jimjam@gmail.com>
2016-09-05 21:46:14 +1200

    commit

---
+++ myScript.js
@@ -0,0 +1,23 @@
+var main = function() {
+  $('iconmenu').click function(){
+    ('.menu').animate({
+      left:"0px"
+    }, 200);
+
+  $('body').animate({
+    left:"340px"
+  }, 200);
+});
+
+$('.icon').click function(){}
+  ('.menu').animate({
+    left:"-340px"
+  }, 200);
+
+  $('body').animate({
+  left:"0px"
+  }, 200);
+});
+
+
+}
